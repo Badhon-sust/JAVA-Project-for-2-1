ee6cf025f7a8164640d2d8518d5eb9e627a90b19
Badhon-sust <52969939+Badhon-sust@users.noreply.github.com>
2021-06-15 06:13:52 +0000

    Update Manage.java

--- Manage.java
+++ Manage.java
@@ -92,3 +92,145 @@ class holder implements Serializable
     Singleroom deluxe_singleerrom[]=new Singleroom[20]; //Deluxe
 }
 
+
+
+
+class Hotel
+{
+    static holder hotel_ob=new holder();
+    static Scanner sc = new Scanner(System.in);
+    static void CustDetails(int i,int rn)
+    {
+        String name, contact, gender;
+        String name2 = null, contact2 = null; 
+        String gender2="";
+        System.out.print("\nEnter customer name: ");
+        name = sc.next();
+        System.out.print("Enter contact number: ");
+        contact=sc.next();
+        System.out.print("Enter gender: ");
+        gender = sc.next();
+        if(i<3)
+        {
+        System.out.print("Enter second customer name: ");
+        name2 = sc.next();
+        System.out.print("Enter contact number: ");
+        contact2=sc.next();
+        System.out.print("Enter gender: ");
+        gender2 = sc.next();
+        }      
+        
+          switch (i) {
+            case 1:hotel_ob.luxury_doublerrom[rn]=new Doubleroom(name,contact,gender,name2,contact2,gender2);
+                break;
+            case 2:hotel_ob.deluxe_doublerrom[rn]=new Doubleroom(name,contact,gender,name2,contact2,gender2);
+                break;
+            case 3:hotel_ob.luxury_singleerrom[rn]=new Singleroom(name,contact,gender);
+                break;
+            case 4:hotel_ob.deluxe_singleerrom[rn]=new Singleroom(name,contact,gender);
+                break;
+            default:System.out.println("Wrong option");
+                break;
+        }
+    }
+    
+    static void bookroom(int i)
+    {
+        int j;
+        int rn;
+        System.out.println("\nChoose room number from : ");
+        switch (i) {
+            case 1:
+                for(j=0;j<hotel_ob.luxury_doublerrom.length;j++)
+                {
+                    if(hotel_ob.luxury_doublerrom[j]==null)
+                    {
+                        System.out.print(j+1+",");
+                    }
+                }
+                System.out.print("\nEnter room number: ");
+                try{
+                rn=sc.nextInt();
+                rn--;
+                if(hotel_ob.luxury_doublerrom[rn]!=null)
+                    throw new NotAvailable();
+                CustDetails(i,rn);
+                }
+                catch(Exception e)
+                {
+                    System.out.println("Invalid Option");
+                    return;
+                }
+                break;
+            case 2:
+                 for(j=0;j<hotel_ob.deluxe_doublerrom.length;j++)
+                {
+                    if(hotel_ob.deluxe_doublerrom[j]==null)
+                    {
+                        System.out.print(j+11+",");
+                    }
+                }
+                System.out.print("\nEnter room number: ");
+                try{
+                rn=sc.nextInt();
+                rn=rn-11;
+                if(hotel_ob.deluxe_doublerrom[rn]!=null)
+                    throw new NotAvailable();
+                CustDetails(i,rn);
+                }
+                catch(Exception e)
+                {
+                    System.out.println("Invalid Option");
+                    return;
+                }
+                break;
+            case 3:
+                  for(j=0;j<hotel_ob.luxury_singleerrom.length;j++)
+                {
+                    if(hotel_ob.luxury_singleerrom[j]==null)
+                    {
+                        System.out.print(j+31+",");
+                    }
+                }
+                System.out.print("\nEnter room number: ");
+                try{
+                rn=sc.nextInt();
+                rn=rn-31;
+                if(hotel_ob.luxury_singleerrom[rn]!=null)
+                    throw new NotAvailable();
+                CustDetails(i,rn);
+                }
+                catch(Exception e)
+                {
+                    System.out.println("Invalid Option");
+                    return;
+                }
+                break;
+            case 4:
+                  for(j=0;j<hotel_ob.deluxe_singleerrom.length;j++)
+                {
+                    if(hotel_ob.deluxe_singleerrom[j]==null)
+                    {
+                        System.out.print(j+41+",");
+                    }
+                }
+                System.out.print("\nEnter room number: ");
+                try{
+                rn=sc.nextInt();
+                rn=rn-41;
+                if(hotel_ob.deluxe_singleerrom[rn]!=null)
+                    throw new NotAvailable();
+                CustDetails(i,rn);
+                }
+                catch(Exception e)
+                {
+                   System.out.println("Invalid Option");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Enter valid option");
+                break;
+        }
+        System.out.println("Room Booked");
+    }
